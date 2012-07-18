f3ca97fa9d77b9dbaedecdb21e54eacad218ed5d
Garrett Heath Koller <garrettheath4@gmail.com>
2012-07-17 23:45:43 -0400

    Created installApps.sh script

--- configure.sh
+++ configure.sh
@@ -48,23 +48,6 @@ for nd in $NORMALDIRS; do
 	fi
 done
 
-# Add fractals to Pictures folder
-if [ -d "$EXPORTS/Fractals-2560x1600" ]; then
-	ln -is "$EXPORTS/Fractals-2560x1600" ~/Pictures/
-fi
-
-# Install special sound effects
-echo "Adding \"Robot Blip\" to system sounds..."
-if [ -e "$EXPORTS/Sounds/Robot Blip.aiff" ]; then
-	sudo cp "$EXPORTS/Sounds/Robot Blip.aiff" /System/Library/Sounds/
-	if [ "$?" != 0 ]; then
-		echo "Warning: Error in installing Robot Blip."
-		echo "         Perhaps you don't have sudo permissions?"
-	fi
-else
-	echo "Warning: Unable to find \"Robot Blip.aiff\"."
-fi
-
 # Install GrowlStyles
 GROWL_FOLDER=~/Library/Application\ Support/Growl/Plugins
 GROWLS="`ls -1 GrowlStyles/`"
@@ -77,16 +60,6 @@ for grr in $GROWLS; do
 		echo "Note: Growl Style \"$grr\" already installed"
 	fi
 done
-
-# Prompt to install Mail ActOn
-read -p "Install Mail ActOn now? (y/n) [y]: " confirmacton
-if [ "$confirmacton" != n ]; then
-	pbcopy < "$EXPORTS/MailActOn/license-key.txt"
-	echo "Mail ActOn Name: Garrett Koller"
-	echo "Mail ActOn Key:  MAO-001-AHN2-J1GJ-FBTS-J4F7-BAP9"
-	echo "   (The license key has been copied to the clipboard.)"
-	open "$EXPORTS/MailActOn/Website.webloc"
-fi
 
 # Install Mail ActOn Rules
 MAILON_FOLDER=~/Library/Mail/Indev
---
+++ installApps.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+EXPORTS="`pwd`"
+
+# Add fractals to Pictures folder
+if [ -d "$EXPORTS/Fractals-2560x1600" ]; then
+	ln -is "$EXPORTS/Fractals-2560x1600" ~/Pictures/
+fi
+
+# Install special sound effects
+read -p "Install \"Robot Blip\" sound now? (y/n) [y]: " confirmsound
+if [ "$confirmsound" != "n" ]; then
+	echo "Adding \"Robot Blip\" to system sounds..."
+	if [ -e "$EXPORTS/Sounds/Robot Blip.aiff" ]; then
+		sudo cp "$EXPORTS/Sounds/Robot Blip.aiff" /System/Library/Sounds/
+		if [ "$?" != 0 ]; then
+			echo "Warning: Error in installing Robot Blip."
+			echo "         Perhaps you don't have sudo permissions?"
+		fi
+	else
+		echo "Warning: Unable to find \"Robot Blip.aiff\"."
+	fi
+fi
+
+# Prompt to install Mail ActOn
+read -p "Install Mail ActOn now? (y/n) [y]: " confirmacton
+if [ "$confirmacton" != "n" ]; then
+	pbcopy < "$EXPORTS/MailActOn/license-key.txt"
+	echo "Mail ActOn Name: Garrett Koller"
+	echo "Mail ActOn Key:  MAO-001-AHN2-J1GJ-FBTS-J4F7-BAP9"
+	echo "   (The license key has been copied to the clipboard.)"
+	open "$EXPORTS/MailActOn/Website.webloc"
+fi
