010d3dd16382ad692c17ce7c8355fa45a6966920
Garrett Koller <gkoller@catalist.us>
2016-05-26 14:55:56 -0400

    Added Git pre-commit hook example script to show project TODOs before committing

---
+++ git-pre-commit-show-todos.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Git pre-commit show-todos script
+
+exclude_from=""
+if [ -f '.gitignore' ]; then
+	exclude_from="--exclude-from='.gitignore'"
+fi
+
+todos=$(grep -ri --exclude='*~' --exclude-dir='.git' $exclude_from '#TODO' .)
+
+if [ -n "$todos" ]; then
+	echo "TODOs in project:"
+	echo "$todos" | sed -e 's/^\.\///' -e 's/^/ * /' -e 's/#TODO//' -e 's/::/:/'
+fi
