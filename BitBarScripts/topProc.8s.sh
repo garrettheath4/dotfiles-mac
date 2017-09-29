768d6182032a1e07a328c37a0cfb2a9678a78ab7
Garrett Heath Koller <garrettheath4@gmail.com>
2017-09-29 11:19:05 -0400

    Minor: Changed CPU usage reporting threshold in topProc script for BitBar

--- BitBarScripts/topProc.8s.sh
+++ BitBarScripts/topProc.8s.sh
@@ -9,10 +9,10 @@
 # Base cmd: ps axrc - displays all %cpu and process names, biggest cpu first
 baseText="$(ps -a -x -r -c -o %cpu,command | head -n4 | tail -n3)"
 
-topCpuPercentInt=$( (head -n1 <<< "$baseText") | egrep -o '[0-9]+' | head -n1)
+topCpuPercentInt=$( (head -n1 <<< "$baseText") | grep -E -o '[0-9]+' | head -n1)
 
 # Short text (for menubar)
-if [ "$topCpuPercentInt" -ge 75 ]; then
+if [ "$topCpuPercentInt" -ge 80 ]; then
 	echo "$( (head -n1 <<< "$baseText") | sed -E -e 's/\.[0-9]+ /% /' -e 's/Google Chrome/Chrome/')|length=10 font='Courier New' size=11"
 else
 	echo "💤|size=11"
