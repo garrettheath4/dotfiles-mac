f886504eb20137d831587b77551293d2d857d03c
Garrett Koller <gkoller@catalist.us>
2024-01-19 12:16:39 -0500

    Patch: Fixed bug in xbar `bamboo.py` script
    where it would display one character at a time from the string `:earth_americas:`
    instead of the string as a whole (to be converted to an earth emoji by xbar)

--- xbar/plugins/bamboo.py
+++ xbar/plugins/bamboo.py
@@ -42,10 +42,10 @@ def get_output_lines():
     """ Main entrypoint for this script. """
 
     if not is_domain_resolvable(BAMBOO_HOSTNAME):
-        return ':earth_americas:'
+        return [':earth_americas:']
 
     if not is_port_open(BAMBOO_HOSTNAME, BAMBOO_PORT):
-        return '🛡'
+        return ['🛡️']
 
     bamboo = Bamboo(BAMBOO_URL)
 
