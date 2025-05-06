5d2f3362ae09f20bdf45fc759f1501d586d8d5b9
Garrett Koller <garrettheath4@gmail.com>
2025-05-05 23:27:17 -0400

    Patch: Fixed bug in `Services/installServices.sh`
    and commented out "Google Chrome" code in `Copy Browser URL.workflow` and `Copy Browser URL as Markdown.workflow`

--- Services/installServices.sh
+++ Services/installServices.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 
+dotfilesServices=$(pwd)
+
 for f in *.workflow ; do
 	# ln: link command
 	# -s: symbolic link
 	# -h: do not follow symbolic links (when creating links)
 	# -i: prompt before overwriting existing files
 	# -v: verbose output
-	ln -shiv "$f" "$HOME/Library/Services/$f"
+	ln -shiv "$dotfilesServices/$f" "$HOME/Library/Services/$f"
 done
